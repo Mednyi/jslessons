e742dfba3d369d88eb85c9efa52af6a5832ba5dd
mytestkube <mednyi.vsadnik@gmail.com>
2020-11-08 14:20:51 +0300

    event listeneres

--- jsDOM.js
+++ jsDOM.js
@@ -24,3 +24,21 @@ parent.appendChild(newEl);
  * Добавить для каждой карточки товара кнопку купить.
  * Написать функцию поиска по товарам.
  */
+// Event handlers
+// Most usable events :
+// General : onclick, onmousedown, onmouseup, onmousemove, onmouseleave, onmouseenter
+// Input: onchange, oninput, onblur, onfocus // onblur or onfocus work for every element with tabindex attribute
+const processInput = function (e) {};
+const myButton = document.createElement('div');
+myButton.onclick = function (e) {
+    console.log(e);
+}
+myButton.addEventListener('click', function (e) {})
+myButton.addEventListener('click', processInput);
+myButton.removeEventListener('click', processInput);
+const myInput = document.createElement('input');
+myInput.oninput = function(e) {
+    console.log(e);
+}
+myInput.oninput = function (e) {console.log(this.value);}
+myInput.addEventListener('input', function (e) {})
